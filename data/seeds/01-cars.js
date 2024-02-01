a4a8f05483c60e8f77de42841a83cd2d445119b3
jovan <jovancraft@gmail.com>
2024-02-01 06:16:39 -0600

    created my car seed info

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,56 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'Toyota',
+        model: 'Corolla',
+        mileage: 32173,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '2222222222222',
+        make: 'Nissan',
+        model: 'GT-R',
+        mileage: 5137,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '3333333333333',
+        make: 'Chrysler',
+        model: 'Saratoga',
+        mileage: 197862,
+        transmission: 'manual'
+    },
+    {
+        vin: '4444444444444',
+        make: 'Ford',
+        model: 'Flex',
+        mileage: 93019,
+        title: 'salvage',
+    },
+    {
+        vin: '5555555555555',
+        make: 'Honda',
+        model: 'Accord',
+        mileage: 73211,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '6666666666666',
+        make: 'Kia',
+        model: 'Forte',
+        mileage: 1218,
+        title: 'clean',
+        transmission: 'automatic'
+    }
+]
+
+
+exports.seed = async function(knex) {
+
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
