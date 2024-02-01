e494480ef33e53e0a364c37435e12fbfd17d6220
jovan <jovancraft@gmail.com>
2024-02-01 07:11:55 -0600

    correctly got the getAll to work correctly and passed the first 2 test specs

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,27 @@
 // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const router = require('express').Router();
+
+
+router.get('/', (req, res) => {
+
+    Cars.getAll()
+    .then(cars => {
+        res.json(cars)
+    })
+    .catch()
+})
+
+router.get('/:id', (req, res) => {
+    console.log('getById wired')
+})
+
+router.post('/', (req, res) => {
+    console.log('create wired')
+})
+
+
+
+
+
+module.exports = router
